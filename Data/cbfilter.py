f239f211ce40bc0e8d244cd52ff088ad748c1aef
giovannibucci <giovi93@hotmail.com>
2016-10-21 13:35:54 +0200

    explode
    bum

--- Data/cbfilter.py
+++ Data/cbfilter.py
@@ -38,7 +38,18 @@ def floatify(str):
         return float(str)
     except ValueError, ex:
         return float(-1)
-		
+
+def extract_title(items):
+	titlev = []
+	for item in items:
+		titles = item[1].split(',')
+		for tit in titles:
+			if tit not in titlev:
+				titlev.append(tit)
+	return titlev
+	
+titles = extract_title(items)
+        
 def extract_tag(items):
 	tagv = []
 	for item in items:
@@ -49,52 +60,115 @@ def extract_tag(items):
 	return tagv
 	
 tags = extract_tag(items)
-print(str(tags))
-	
+
+def extract_discipline(items):
+   disciplinesv = []
+   for item in items:
+      disc = intify(item[3])
+      if disc not in disciplinesv:
+            disciplinesv.append(disc)
+            
+   return disciplinesv
+   
+disciplines = extract_discipline(items)
+
+def extract_industry(items):
+   industriesv = []
+   for item in items:
+      industry = intify(item[4])
+      if industry not in industriesv:
+            industriesv.append(industry)
+   return industriesv
+   
+industries = extract_industry(items)
 
 def convertItem(item):
     newItem = []
     newItem.append(intify(item[0]))
-    newItem.append(item[1].split(","))
-    newItem.append(intify(item[2]))
-    newItem.append(intify(item[3]))
-    newItem.append(intify(item[4]))
+    
+    titletemp = item[1].split(",")
+    for title in titles:
+      if title in titletemp:
+         newItem.append(1)
+      else:
+        newItem.append(0)
+        
+    for i in range[1,7]:
+      if i == intify(item[2]):
+         newItem.append(1)
+      else:
+         newItem.append(0)
+         
+    for disc in disciplines:
+      if intify(item[3]) == disc:
+         newItem.append(1)
+      else: 
+         newItem.append(0)
+      
+    for industry in industries:
+      if intify(item[4]) == indusrty:
+         newItem.append(1)
+      else:
+         newItem.append(0)
+    
     if item[5] == 'de':
-        newItem.append(1)
-		newItem.append(0)
+      newItem.append(1)
+      newItem.append(0)
 		newItem.append(0)
 		newItem.append(0)
     elif item[5] == 'at':
 		newItem.append(0)
-        newItem.append(1)
+      newItem.append(1)
 		newItem.append(0)
 		newItem.append(0)
     elif item[5] == 'ch':
 		newItem.append(0)
 		newItem.append(0)
-        newItem.append(1)
+      newItem.append(1)
 		newItem.append(0)
     elif item[5] == 'non_dach':
-        newItem.append(0)
+      newItem.append(0)
 		newItem.append(0)
 		newItem.append(0)
 		newItem.append(1)
     else:
-        newItem.append(0)
+      newItem.append(0)
 		newItem.append(0)
 		newItem.append(0)
 		newItem.append(0)
-    newItem.append(intify(item[6]))
+    
+      for i in range[0,17]:
+         if i == intify(item[6])
+            newItem.append(1)
+         else:
+            newItem.append(0)
+            
     newItem.append(floatify(item[7]))
     newItem.append(floatify(item[8]))
-    newItem.append(intify(item[9]))
-    newItem.append(item[10].split(","))
+    
+    for i in range[0,6]
+      if intify(item[9]) == i
+         newItem.append(1)
+      else 
+         newItem.append(0)
+      
+    
+    tagtemp = item[10].split(',')
+    for tag in tags:
+      if tag in tagtemp:
+        newItem.append(1)
+      else:
+        newItem.append(0)
+        
     newItem.append(intify(item[11]))
     newItem.append(intify(item[12]))
     return newItem
 
+matrix = open('matrix.csv','w')
 #Reshape array, convert to int
 for i in range(0,len(items)):
     items[i] = convertItem(items[i])
+    matrix.write(items[i])
+
 
 
