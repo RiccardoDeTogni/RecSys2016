c4aea49d754c42a33128e72d2922f6556f20dc58
RiccardoDeTogni <riccardo.detogni@mail.polimi.it>
2016-10-22 17:22:04 +0200

    Downgrade

--- Data/cbfilter.py
+++ Data/cbfilter.py
@@ -2,6 +2,7 @@
 import time
 import numpy as np
 import csv
+import os
 from collections import defaultdict
 from multiprocessing.dummy import Pool as ThreadPool
 from functools import partial
@@ -38,137 +39,167 @@ def floatify(str):
         return float(str)
     except ValueError, ex:
         return float(-1)
+		
+titlev = []
+tagv = []
+disciplinesv = []
+industriesv = []
 
-def extract_title(items):
-	titlev = []
-	for item in items:
-		titles = item[1].split(',')
-		for tit in titles:
-			if tit not in titlev:
-				titlev.append(tit)
-	return titlev
-	
-titles = extract_title(items)
-        
-def extract_tag(items):
-	tagv = []
+if not os.path.isfile('tags.csv') or not os.path.isfile('industries.csv') or not os.path.isfile('titles.csv') or not os.path.isfile('disciplines.csv'):    
+
+
+	#extract all possible features
+
 	for item in items:
 		tags = item[10].split(',')
 		for tag in tags:
 			if tag not in tagv:
-				tagv.append(tag)
-	return tagv
+				tagv.append(intify(tag))
+		
+		disc = intify(item[3])
+		if disc not in disciplinesv:
+			disciplinesv.append(disc)
+			
+		industry = intify(item[4])
+		if industry not in industriesv:
+			industriesv.append(industry)
+		
+		titles = item[1].split(',')
+		for tit in titles:
+			if tit not in titlev:
+				titlev.append(intify(tit))
+
+	print 'features retrievied...'
+	tags = open('tags.csv','w')
+	titles = open('titles.csv','w')
+	disciplines = open('disciplines.csv','w')
+	industries = open('industries.csv','w')
+
+	#caching features
+	for tag in tagv:
+		tags.write(str(tag) + '\n')
+	for title in titlev:
+		titles.write(str(title) + '\n')
+	for discipline in disciplinesv:
+		disciplines.write(str(discipline) + '\n')
+	for industry in industriesv:
+		industries.write(str(industry) + '\n')
+else:
+	tags = open('tags.csv','r')
+	titles = open('titles.csv','r')
+	disciplines = open('disciplines.csv','r')
+	industries = open('industries.csv','r')
 	
-tags = extract_tag(items)
+	for line in tags:
+		tagv.append(intify(line))
+	for line in titles:
+		titlev.append(intify(line))
+	for line in disciplines:
+		disciplinesv.append(intify(line))
+	for line in industries:
+		industriesv.append(intify(line))
+	print 'features retrievied...'
+	
 
-def extract_discipline(items):
-   disciplinesv = []
-   for item in items:
-      disc = intify(item[3])
-      if disc not in disciplinesv:
-            disciplinesv.append(disc)
-            
-   return disciplinesv
-   
-disciplines = extract_discipline(items)
-
-def extract_industry(items):
-   industriesv = []
-   for item in items:
-      industry = intify(item[4])
-      if industry not in industriesv:
-            industriesv.append(industry)
-   return industriesv
-   
-industries = extract_industry(items)
+tags.close()
+titles.close()
+disciplines.close()
+disciplines.close()
+industries.close()	
 
 def convertItem(item):
-    newItem = []
-    newItem.append(intify(item[0]))
+	newItem = []
+	newItem.append(intify(item[0]))
     
-    titletemp = item[1].split(",")
-    for title in titles:
-      if title in titletemp:
-         newItem.append(1)
-      else:
-        newItem.append(0)
-        
-    for i in range[1,7]:
-      if i == intify(item[2]):
-         newItem.append(1)
-      else:
-         newItem.append(0)
+	titletemp = item[1].split(",")
+	for title in titletemp:
+		intify(title)
+		
+	for title in titlev:
+		if title in titletemp:
+			newItem.append(1)
+		else:
+			newItem.append(0)
+		
+	for i in range(1,7):
+		if i == intify(item[2]):
+			newItem.append(1)
+		else:
+			newItem.append(0)
          
-    for disc in disciplines:
-      if intify(item[3]) == disc:
-         newItem.append(1)
-      else: 
-         newItem.append(0)
+	for disc in disciplinesv:
+		if intify(item[3]) == disc:
+			newItem.append(1)
+		else: 
+			newItem.append(0)
       
-    for industry in industries:
-      if intify(item[4]) == indusrty:
-         newItem.append(1)
-      else:
-         newItem.append(0)
+	for industry in industriesv:
+		if intify(item[4]) == industry:
+			newItem.append(1)
+		else:
+			newItem.append(0)
     
-    if item[5] == 'de':
-      newItem.append(1)
-      newItem.append(0)
+	if item[5] == 'de':
+		newItem.append(1)
 		newItem.append(0)
 		newItem.append(0)
-    elif item[5] == 'at':
 		newItem.append(0)
-      newItem.append(1)
+	elif item[5] == 'at':
 		newItem.append(0)
-		newItem.append(0)
-    elif item[5] == 'ch':
+		newItem.append(1)
 		newItem.append(0)
 		newItem.append(0)
-      newItem.append(1)
-		newItem.append(0)
-    elif item[5] == 'non_dach':
-      newItem.append(0)
+	elif item[5] == 'ch':
 		newItem.append(0)
 		newItem.append(0)
 		newItem.append(1)
-    else:
-      newItem.append(0)
+		newItem.append(0)
+	elif item[5] == 'non_dach':
+		newItem.append(0)
+		newItem.append(0)
+		newItem.append(0)
+		newItem.append(1)
+	else:
+		newItem.append(0)
 		newItem.append(0)
 		newItem.append(0)
 		newItem.append(0)
     
-      for i in range[0,17]:
-         if i == intify(item[6])
-            newItem.append(1)
-         else:
-            newItem.append(0)
+	for i in range(0,17):
+		if i == intify(item[6]):
+			newItem.append(1)
+		else:
+			newItem.append(0)
             
-    newItem.append(floatify(item[7]))
-    newItem.append(floatify(item[8]))
+	newItem.append(floatify(item[7]))
+	newItem.append(floatify(item[8]))
     
-    for i in range[0,6]
-      if intify(item[9]) == i
-         newItem.append(1)
-      else 
-         newItem.append(0)
+	for i in range(0,6):
+		if intify(item[9]) == i:
+			newItem.append(1)
+		else :
+			newItem.append(0)
       
     
-    tagtemp = item[10].split(',')
-    for tag in tags:
-      if tag in tagtemp:
-        newItem.append(1)
-      else:
-        newItem.append(0)
+	tagtemp = item[10].split(',')
+	for tag in tagv:
+		if tag in tagtemp:
+			newItem.append(1)
+		else:
+			newItem.append(0)
         
-    newItem.append(intify(item[11]))
-    newItem.append(intify(item[12]))
-    return newItem
+	newItem.append(intify(item[11]))
+	newItem.append(intify(item[12]))
+	return newItem
 
 matrix = open('matrix.csv','w')
 #Reshape array, convert to int
-for i in range(0,len(items)):
-    items[i] = convertItem(items[i])
-    matrix.write(items[i])
+for item in items:
+	i = 0
+    item = convertItem(item)
+    matrix.write(str(item))
+	print Expanded element + str(i)
+	i += 1
 
 
 
